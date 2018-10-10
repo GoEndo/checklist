c39b11105a9ad109681ceb33ad92ad83a2202f3f
Go Endo <go.endo@icloud.com>
2018-10-10 12:11:10 -0500

    12.4 Email in production (take two)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_125446) do
+ActiveRecord::Schema.define(version: 2018_10_10_155857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2018_10_01_125446) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
